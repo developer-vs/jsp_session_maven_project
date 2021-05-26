49bf469213cc0e4ed9a647be1feaecad3a1fe60f
Vladimir Semenovich <vladimirgdsr@gmail.com>
2021-05-26 15:10:09 -0700

    added credentials validation

--- HerokuSessionApp/WebContent/member-area.jsp
+++ HerokuSessionApp/WebContent/member-area.jsp
@@ -7,14 +7,19 @@
 <title>Member Area</title>
 </head>
 <body>
-	<% String userName = (String)request.getSession().getAttribute("userName"); %>
-	<p>Hello, <%= userName %>!!!</p>
-	<% 
+	<%
+	    String userName = "guest";
 		// getSession(false) - don't create if it doesn't exist
+		// false to return null if there's no current session
 		HttpSession newSession = request.getSession(false);
 	
-		int timeout = newSession.getMaxInactiveInterval();	
-	
+		if(newSession != null) {
+			userName = (String)newSession.getAttribute("userName");
+		}
+	%>
+		<p>Hello, <%= userName %>!!!</p>
+	<% 	
+		int timeout = newSession.getMaxInactiveInterval();		
 		out.print(timeout + " sec left");
 		response.setHeader("Refresh", timeout + "; URL = index.jsp");		
 	%>
--- HerokuSessionApp/src/com/herokuapps/session/SiteController.java
+++ HerokuSessionApp/src/com/herokuapps/session/SiteController.java
@@ -1,11 +1,16 @@
 package com.herokuapps.session;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 /**
  * Servlet implementation class SiteController
@@ -19,17 +24,29 @@ public class SiteController extends HttpServlet {
      */
     public SiteController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String userName = (String) request.getAttribute("userName");
-		String password = (String) request.getAttribute("password");
+		String userName = (String) request.getParameter("userName");
+		String password = (String) request.getParameter("password");
 		
-		response.sendRedirect("member-area.jsp");
+		if (userName.equals("admin") && password.equals("12345")) {
+			// Invalidates this session then unbinds any objects bound to it.
+			request.getSession().invalidate();
+			
+			// create - true to create a new session for this request if necessary; f
+			// false to return null if there's no current session
+			HttpSession session = request.getSession(true);
+			session.setMaxInactiveInterval(10); // 10 sec
+			session.setAttribute("userName", userName);
+			response.sendRedirect("member-area.jsp");
+		}
+		else {
+			response.sendRedirect("index.jsp");
+		}
 	}
 
 }
